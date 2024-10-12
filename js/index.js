a55af34fc824dbca33fb00715e2926787d4fc6b8
LeQuangHieuVKU <hieulq.23itb@vku.udn.vn>
2024-10-12 14:31:47 +0700

    first-project

---
+++ js/index.js
@@ -0,0 +1,1 @@
+/**admin/user active list */
---
+++ php/signup.php
@@ -0,0 +1,48 @@
+<?php
+
+if (
+	isset($_POST['fname']) &&
+	isset($_POST['uname']) &&
+	isset($_POST['pass'])
+) {
+
+	include "../db_conn.php";
+
+	$fname = $_POST['fname'];
+	$uname = $_POST['uname'];
+	$pass = $_POST['pass'];
+
+	$data = "fname=" . $fname . "&uname=" . $uname;
+
+	if (empty($fname)) {
+		$em = "Full name is required";
+		header("Location: ../signup.php?error=$em&$data");
+		exit;
+	} else if (empty($uname)) {
+		$em = "User name is required";
+		header("Location: ../signup.php?error=$em&$data");
+		exit;
+	} else if (empty($pass)) {
+		$em = "Password is required";
+		header("Location: ../signup.php?error=$em&$data");
+		exit;
+	} else {
+		// hashing the password
+		$pass = password_hash($pass, PASSWORD_DEFAULT);
+
+		try {
+			$sql = "INSERT INTO users(fname, username, password) VALUES(?,?,?)";
+			$stmt = $conn->prepare($sql);
+			$stmt->execute([$fname, $uname, $pass]);
+
+			header("Location: ../signup.php?success=Your account has been created successfully");
+			exit;
+		} catch (PDOException $e) {
+			// Log or display the error message
+			echo "Error: " . $e->getMessage();
+		}
+	}
+} else {
+	header("Location: ../signup.php?error=error");
+	exit;
+}
